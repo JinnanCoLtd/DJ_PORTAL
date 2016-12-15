6ef8e7262d11fa5fa55f41577c17c1315d2d79ce
JinnanCoLtd <murata@jinnan.co.jp>
2016-12-15 12:52:02 +0900

    add gems for heroku

--- app/models/photograph.rb
+++ app/models/photograph.rb
@@ -1,4 +1,4 @@
-class Photograph < ApplicationRecord::Base
+class Photograph < ActiveRecord::Base
   belongs_to :listing
   
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
